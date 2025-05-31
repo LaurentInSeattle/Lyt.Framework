0947cc11aba655cfed2974ca759f7b7711780f29
Laurent <ly.testud@outlook.com>
2025-05-31 09:02:43 -0700

    Updated nugets and some project references. UI still broken

---
+++ Lyt.Framework.Interfaces/Messaging/NullMessenger.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Lyt.Framework.Interfaces.Messaging;
+
+public sealed class NullMessenger : IMessenger
+{
+    public void Publish<TMessage>(TMessage message) 
+        where TMessage : class { }
+
+    public void Subscribe<TMessage>(Action<TMessage> action, bool withUiDispatch = false, bool doNotLog = false) 
+        where TMessage : class { } 
+
+    public void Unregister(object recipient) { }
+
+    public void Unregister<TMessage>(object recipient) 
+        where TMessage : class { }
+}
