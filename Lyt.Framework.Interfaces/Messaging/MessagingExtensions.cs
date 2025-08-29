bef4deef929e4e91a2a4715f6e37dc6b1fa93d4a
Laurent <ly.testud@outlook.com>
2025-08-29 13:32:39 -0700

    Null Messenger

--- Lyt.Framework.Interfaces/Messaging/MessagingExtensions.cs
+++ Lyt.Framework.Interfaces/Messaging/MessagingExtensions.cs
@@ -4,27 +4,22 @@ using CommunityToolkit.Mvvm.Messaging;
 
 public static class MessagingExtensions
 {
+    static MessagingExtensions() => Messenger = WeakReferenceMessenger.Default;
+
+    public static IMessenger Messenger { get; set; }
+
     public static void Subscribe<TMessage>(this IRecipient<TMessage> subscriber) 
         where TMessage : class 
-        => WeakReferenceMessenger.Default.Register<TMessage>(subscriber);
+        => Messenger.Register<TMessage>(subscriber);
 
     public static void Publish<TMessage>(this TMessage message)
         where TMessage : class
-    {
-        ArgumentNullException.ThrowIfNull(message);
-        WeakReferenceMessenger.Default.Send<TMessage>(message);
-    }
+        => Messenger.Send<TMessage>(message);
 
-    public static void Unregister(object recipient)
-    {
-        ArgumentNullException.ThrowIfNull(recipient);
-        WeakReferenceMessenger.Default.UnregisterAll(recipient);
-    }
+    public static void Unregister(this object recipient)
+        => Messenger.UnregisterAll(recipient);
 
     public static void Unregister<TMessage>(this object recipient)
         where TMessage : class
-    {
-        ArgumentNullException.ThrowIfNull(recipient);
-        WeakReferenceMessenger.Default.Unregister<TMessage>(recipient);
-    }
+        => Messenger.Unregister<TMessage>(recipient);
 }
---
+++ Lyt.Framework.Interfaces/Messaging/NullMessenger.cs
@@ -0,0 +1,36 @@
+﻿namespace Lyt.Framework.Interfaces.Messaging;
+
+using CommunityToolkit.Mvvm.Messaging;
+
+public sealed class NullMessenger : IMessenger
+{
+    public void Reset() { }
+
+    public void Cleanup() { }
+    
+    public void Register<TRecipient, TMessage, TToken>(
+        TRecipient recipient, TToken token, MessageHandler<TRecipient, TMessage> handler)
+        where TRecipient : class
+        where TMessage : class
+        where TToken : IEquatable<TToken>
+    { 
+    }
+
+    public bool IsRegistered<TMessage, TToken>(object recipient, TToken token)
+        where TMessage : class
+        where TToken : IEquatable<TToken>
+        => false;
+
+    public TMessage Send<TMessage, TToken>(TMessage message, TToken token)
+        where TMessage : class
+        where TToken : IEquatable<TToken>
+        => message;
+
+    public void Unregister<TMessage, TToken>(object recipient, TToken token)
+        where TMessage : class
+        where TToken : IEquatable<TToken> { }
+
+    public void UnregisterAll(object recipient) { }
+
+    public void UnregisterAll<TToken>(object recipient, TToken token) where TToken : IEquatable<TToken> { }
+}
