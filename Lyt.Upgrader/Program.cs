f36bc67a52a48e68c0d57520035c8b63f9878b0c
Laurent <ly.testud@outlook.com>
2025-09-17 10:09:57 -0700

    Upgraded to .Net 10

---
+++ Lyt.Upgrader/Program.cs
@@ -0,0 +1,44 @@
+﻿#define LYT_DEBUG 
+
+namespace Lyt.Upgrader;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Upgrader");
+
+#if DEBUG
+#if LYT_DEBUG 
+        string path = "C:\\Users\\Laurent\\source\\repos";
+        // string path = @"C:\Users\Laurent\Desktop\Code"; 
+        // string path = @"C:\Users\Laurent\source\repos";
+        string[] debugArgs = [path];
+        args = debugArgs;
+#endif
+#endif
+
+        var upgrade = new Upgrade();
+        if (upgrade.Initialize(args))
+        {
+            if (upgrade.Run())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Upgrader success");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Upgrader error: Failed to complete.");
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Failed to initialize Upgrader");
+        }
+
+        Console.ReadLine();
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
---
+++ Lyt.Upgrader/Upgrade.cs
@@ -0,0 +1,121 @@
+﻿namespace Lyt.Upgrader;
+
+internal class Upgrade
+{
+    private readonly EnumerationOptions enumerationOptions;
+    private string rootPath;
+
+    private const string source = "<TargetFramework>net9.0</TargetFramework>";
+    private const string target = "<TargetFramework>net10.0</TargetFramework>";
+
+    public Upgrade()
+    {
+        this.enumerationOptions = new EnumerationOptions()
+        {
+            IgnoreInaccessible = false,
+            RecurseSubdirectories = false,
+            MatchType = MatchType.Simple,
+            MaxRecursionDepth = 8,
+        };
+
+        this.rootPath = string.Empty;
+    }
+
+    internal bool Initialize(string[] parameters)
+    {
+        if (parameters is null || parameters.Length == 0)
+        {
+            return false;
+        }
+
+        string maybeRootPath = parameters[0];
+        if (string.IsNullOrWhiteSpace(maybeRootPath) || !Path.Exists(maybeRootPath))
+        {
+            return false;
+        }
+
+        this.rootPath = maybeRootPath;
+        return true;
+    }
+
+    internal bool Run()
+    {
+        bool success;
+        try
+        {
+            success = this.ProcessDirectory(this.rootPath);
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Exception thrown: \n" + ex);
+            success = false;
+        }
+
+        return success;
+    }
+
+    private bool ProcessDirectory(string folderPath)
+    {
+        static bool UpgradeProjectFile(string projectFile)
+        {
+            bool edited = false;
+            string[] lines = File.ReadAllLines(projectFile);
+            List<string> newLines = [];
+            foreach (string line in lines)
+            {
+                string newLine = line;
+                if (line.Contains(source, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    newLine = line.Replace(source, target);
+                    edited = true;
+                }
+
+                newLines.Add(newLine);
+            }
+
+            if (edited)
+            {
+                File.WriteAllLines(projectFile, newLines);
+            }
+
+            return edited;
+        }
+
+        try
+        {
+            // Check if the folder contains a *.csproj file
+            var csProjFiles = folderPath.EnumerateFiles(this.enumerationOptions, "*.csproj");
+            if (csProjFiles.Count > 0)
+            {
+                foreach (string projectFile in csProjFiles)
+                {
+                    if (UpgradeProjectFile(projectFile))
+                    {
+                        Console.WriteLine("Upgraded: " + projectFile);
+                    }
+                }
+            }
+
+            // Recurse to sub folders, except '.git'
+            var subDirs = folderPath.EnumerateDirectories();
+            foreach (string subDir in subDirs)
+            {
+                if (subDir.Contains(".git", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    continue;
+                }
+
+                this.ProcessDirectory(subDir);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception thrown: \n" + ex);
+            throw;
+        }
+
+        return true;
+    }
+}
