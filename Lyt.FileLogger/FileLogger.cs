dc780facaf4116f933b43ab763d7354d01a2e6a0
Laurent <ly.testud@outlook.com>
2026-09-18 09:40:30 -0700

    File logger to log in app data

--- Lyt.FileLogger/FileLogger.cs
+++ Lyt.FileLogger/FileLogger.cs
@@ -4,29 +4,42 @@ using Serilog;
 
 public sealed class FileLogger : Lyt.Framework.Interfaces.Logging.ILogger
 {
+    public static string AppName = string.Empty;
+
     public FileLogger()
     {
+        if (string.IsNullOrWhiteSpace(AppName))
+        {
+            throw new Exception("App Name is mising");
+        }
+
+        const string PublisherFolder = "Lyt";
+        const string LogsFolder = "Logs";
+        // local app data store
+        string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        string fileName = string.Format("{0}Log.txt", AppName);
+        string filePath = Path.Combine(localAppData, PublisherFolder, AppName, LogsFolder, fileName);
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .WriteTo.File(
-                "Logs/PhotoRebelLog.txt",
+                filePath,
                 rollingInterval: RollingInterval.Day,
                 retainedFileTimeLimit: TimeSpan.FromDays(3),
                 retainedFileCountLimit: 10,
                 fileSizeLimitBytes: 256 * 1024 * 1024)
-            .CreateLogger(); 
+            .CreateLogger();
 
     }
 
-    public bool BreakOnError { get => false; set { } } 
+    public bool BreakOnError { get => false; set { } }
 
     public void Debug(string message) => Log.Debug(message);
 
     public void Info(string message) => Log.Information(message);
 
-    public void Warning(string message) => Log.Warning(message); 
+    public void Warning(string message) => Log.Warning(message);
 
     public void Error(string message) => Log.Error(message);
 
-    public void Fatal(string message) => Log.Fatal(message); 
+    public void Fatal(string message) => Log.Fatal(message);
 }
